e236e178e8db41665bbe79d6ae0d84946aa489dc
front-end19 <103452351+front-end19@users.noreply.github.com>
2022-08-15 10:47:34 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
 const arrows = document.querySelectorAll("i")
 arrows.forEach((arrow) => {
-    arrow.addEventListener("click", (eo) => {
-        const answer = eo.target.parentElement.parentElement.getElementsByClassName("answer")[0];
-        answer.classList.toggle("show-answer");
-        if (answer.classList.contains("show-answer")) {
-            eo.target.setAttribute("class", "fa-solid fa-angle-up");
-        } else {
-            eo.target.setAttribute("class", "fa-solid fa-angle-down");
-        }
+        arrow.addEventListener("click",function (eo) {
+        arrows.forEach(function (a) {
+            if(a===eo.target){
+                a.parentElement.parentElement.getElementsByClassName("answer")[0].classList.toggle("show-answer");
+            }else{
+                a.parentElement.parentElement.getElementsByClassName("answer")[0].classList.remove("show-answer")
+            }
+            if (a.parentElement.parentElement.getElementsByClassName("answer")[0].classList.contains("show-answer")) {
+                a.setAttribute("class", "fa-solid fa-angle-up");
+            } else {
+                a.setAttribute("class", "fa-solid fa-angle-down");
+            }
+        })
     })
 })
